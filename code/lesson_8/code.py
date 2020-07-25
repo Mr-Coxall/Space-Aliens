0228562a1c78f2f497f51e93c07e03b50a2a3128
Mr. Coxall <mr.coxall@mths.ca>
2020-07-24 23:28:03 -0400

    lesson 8

--- code/lesson_8/code.py
+++ code/lesson_8/code.py
@@ -6,10 +6,45 @@
 
 import ugame
 import stage
+import random
+import time
 
 import constants
 
 
+def splash_scene():
+    # this function is the splash scene game loop
+    
+    # get sound ready
+    coin_sound = open("coin.wav", 'rb')
+    sound = ugame.audio
+    sound.stop()
+    sound.mute(False)
+    sound.play(coin_sound)
+
+    # an image bank for CircuitPython
+    image_bank_mt_background = stage.Bank.from_bmp16("mt_game_studio.bmp")
+
+    # sets the background to image 0 in the bank
+    background = stage.Grid(image_bank_mt_background, constants.SCREEN_X, 
+                            constants.SCREEN_Y)
+
+    # create a stage for the background to show up on
+    #   and set the frame rate to 60fps
+    game = stage.Stage(ugame.display, constants.FPS)
+    # set the layers, items show up in order
+    game.layers = [background]
+    # render the background and inital location of sprite list
+    # most likely you will only render background once per scene
+    game.render_block()
+
+    # repeat forever, game loop
+    while True:
+        # Wait for 1 seconds
+        time.sleep(1.0)
+        menu_scene()
+        
+        
 def menu_scene():
     # this function is the menu scene
     
@@ -35,6 +70,36 @@ def menu_scene():
     # sets the background to image 0 in the image bank
     background = stage.Grid(image_bank_mt_background, constants.SCREEN_X, 
                             constants.SCREEN_Y)
+                            
+    # used this program to split the image into tile: 
+    #   https://ezgif.com/sprite-cutter/ezgif-5-818cdbcc3f66.png
+    background.tile(2, 2, 0)  # blank white
+    background.tile(3, 2, 1)
+    background.tile(4, 2, 2)
+    background.tile(5, 2, 3)
+    background.tile(6, 2, 4)
+    background.tile(7, 2, 0)  # blank white
+
+    background.tile(2, 3, 0)  # blank white
+    background.tile(3, 3, 5)
+    background.tile(4, 3, 6)
+    background.tile(5, 3, 7)
+    background.tile(6, 3, 8)
+    background.tile(7, 3, 0)  # blank white
+
+    background.tile(2, 4, 0)  # blank white
+    background.tile(3, 4, 9)
+    background.tile(4, 4, 10)
+    background.tile(5, 4, 11)
+    background.tile(6, 4, 12)
+    background.tile(7, 4, 0)  # blank white
+
+    background.tile(2, 5, 0)  # blank white
+    background.tile(3, 5, 0)
+    background.tile(4, 5, 13)
+    background.tile(5, 5, 14)
+    background.tile(6, 5, 0)
+    background.tile(7, 5, 0)  # blank white
     
     # create a stage for the background to show up on
     #   and set the frame rate to 60fps
@@ -83,6 +148,10 @@ def game_scene():
     # sets the background to image 0 in the image bank
     background = stage.Grid(image_bank_background, constants.SCREEN_X, 
                             constants.SCREEN_Y)
+    for x_location in range(constants.SCREEN_GRID_X):
+        for y_location in range(constants.SCREEN_GRID_Y):
+            tile_picked = random.randint(1, 3)
+            background.tile(x_location, y_location, tile_picked)
     
     # create a sprite
     # parameters (image_bank, image # in bank, x, y)
@@ -156,4 +225,4 @@ def game_scene():
 
 
 if __name__ == "__main__":
-    menu_scene()
+    splash_scene()
