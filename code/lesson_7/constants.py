bf93ec285b18abdc6a6e4dc795fb29ad47a07aa9
Patrick Coxall <mr.coxall@mths.ca>
2020-12-11 10:32:29 -0500

    Update constants.py

--- code/lesson_7/constants.py
+++ code/lesson_7/constants.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # Created by: Mr. Coxall
-# Created on: July 2020
+# Created on: Sep 2020
 # This constants file is for Space Alien game
 
 # PyBadge screen size is 160x128 and sprites are 16x16
@@ -21,3 +21,7 @@ button_state = {
   "button_still_pressed": "still pressed",
   "button_released": "released"
 }
+
+# new pallet for red filled text
+RED_PALETTE = (b'\xff\xff\x00\x22\xcey\x22\xff\xff\xff\xff\xff\xff\xff\xff\xff'
+               b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff')
